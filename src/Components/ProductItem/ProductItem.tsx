19e0bcc53f689a634b3d2a07cc87ca1d45e31397
Анатолий Косолапов <anatolii@MacBook-Air-Anatolij.local>
2023-09-06 16:53:58 +0300

    add popap window to deleting products from product's llist

--- src/Components/ProductItem/ProductItem.tsx
+++ src/Components/ProductItem/ProductItem.tsx
@@ -1,14 +1,21 @@
+import Modal from 'react-modal';
 import trash from '../../images/icon-trash.svg';
 import { useAppDispatch } from '../../store/hooks';
 import { productSlice } from '../../store/reducers/ProductSlice';
 import { Product } from '../../Types/Product';
 import { prepeadProductDate } from '../../utils/dateFormaters';
+import { MouseEventHandler, useCallback, useState } from 'react';
+import { OrderedProduct } from '../OrderedProduct';
+
+Modal.setAppElement('#root');
 
 interface Props {
   product: Product;
 }
 
 export const ProductItem: React.FC<Props> = ({ product }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const {
     id,
     isNew,
@@ -19,13 +26,19 @@ export const ProductItem: React.FC<Props> = ({ product }) => {
     order
   } = product;
 
+  const openModal = useCallback((event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    setIsModalOpen(true);
+  }, []);
+
+  const closeModal = useCallback(() => setIsModalOpen(false), []);
+
   const firstPrice = product.price.find((price) => price.isDefault === 1);
   const secondPrice = product.price.find((price) => price.isDefault === 0);
 
   const dispatch = useAppDispatch();
 
-  const handleDelete = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    event.preventDefault();
+  const handleDelete: MouseEventHandler<HTMLButtonElement> = () => {
     dispatch(productSlice.actions.removeProduct(id));
   };
 
@@ -86,10 +99,43 @@ export const ProductItem: React.FC<Props> = ({ product }) => {
       <a
         href="#"
         className="products-item__delete"
-        onClick={handleDelete}
+        onClick={openModal}
       >
         <img src={trash} alt="delete button" />
       </a>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Delete Product Modal"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div className="modal__head">
+          <h2 className="modal__head--title">Вы уверены что хотите удалить этот приход?</h2>
+          <hr />
+        </div>
+
+        <div className="modal__body d-flex flex-column">
+          <OrderedProduct product={product} />
+        </div>
+
+        <div className="modal__footer d-flex align-items-center justify-content-end gap-3">
+          <button
+            className="modal__footer--button"
+            onClick={closeModal}
+          >
+            отменить
+          </button>
+
+          <button
+            className="modal__footer--button"
+            onClick={handleDelete}
+          >
+            удалить
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
